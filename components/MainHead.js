c4f0a8c3fd24555328e2cb44e0fe7f6b8daa1463
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-03-08 17:59:33 +0900

    make_banner

---
+++ components/MainHead.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+function MainHead() {
+    return (
+        <View style={styles.block}>
+            <Text style={styles.appName}>HealthApp</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    block : {
+        padding:16,
+        backgroundColor: 'white'
+    },
+    appName: {
+        fontSize:22,
+        color:'black',
+    }
+})
+
+
+export default MainHead;
