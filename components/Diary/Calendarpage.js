f201a078102a0abd07067702c5583fe2874ab1c9
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-04-25 15:50:53 +0900

    erase the default button TT

--- components/Diary/Calendarpage.js
+++ components/Diary/Calendarpage.js
@@ -1,18 +1,9 @@
-import {Text, TextInput,StyleSheet,Modal,Pressable,ScrollView,Button } from 'react-native';
+import {Text,View, TextInput,StyleSheet,Modal,ScrollView,Button } from 'react-native';
 import {Form} from 'react-native-form-component';
 import React,{useState} from 'react'
 import {Calendar} from 'react-native-calendars';
 import axios from 'axios';
-import moment from 'moment';
-import API_URL from '../../config/constants';
 
-export function changeFormat(date) {
-  if (moment(date).isValid()) {
-    return moment(date).format('YYYY-MM-DD');
-  } else {
-    return '2022-04-21';
-  }
-}
 
 const Calendarpage = () => {
 
@@ -20,7 +11,7 @@ const Calendarpage = () => {
   const [modalVisible,setModalVisible]=useState(false);
   const [description, setdescription]=useState('');
 
-  const handleSubmit =async() => {
+  const HandleSubmit =async() => {
     try{
       const resp = await axios.post('http://10.0.2.2:8080/diary',
       {
@@ -37,56 +28,64 @@ const Calendarpage = () => {
 
   return (
     <ScrollView>
-      <Modal
-        animationType = "slide"
-        transparent={true}
-        visible = {modalVisible}
-        onRequestClose = {()=> {
-          console.log('Modal 닫힘');
-          setModalVisible(!modalVisible);
-        }}>
-          <Form onSubmit = {handleSubmit}>
-        <Pressable
-        style={styles.box}
-        onPress = {()=>setModalVisible(!modalVisible)}>
-        <Text 
-        value={date}>해당날짜 : {date}</Text>
-        <TextInput
-          multiline
-          style={styles.input}
-          numberOfLines={8}
-          placeholder='오늘 운동을 계획하고 반성해보세요'
-          onChangeText={(description)=>setdescription(description)}
-          value={description}
-          />
-        </Pressable>
-        </Form>
-        <Button
-          onPress={handleSubmit}
-          title="Submit!"
-          style={styles.button}/>
-      </Modal>
-      <Text>날짜를 클릭해 일기를 확인해보세요!</Text>
-      <Calendar
-        style={styles.customStyle}
-        // blurRadius={this.modalVisible ? 4: 0}
-        onDayPress={(day)=>{
-          setDate(day.dateString)
-          setModalVisible(true)
-        }
-       }/>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Modal
+          style={styles.modal}
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            console.log('Modal 닫힘');
+            setModalVisible(!modalVisible);
+          }}>
+          <View
+            style={styles.box}
+            onPress={() => setModalVisible(!modalVisible)}>
+            <Form buttonText="" buttonStyle={{width: 0}}>
+              <Text value={date}>해당날짜 : {date}</Text>
+              <TextInput
+                multiline
+                style={styles.input}
+                numberOfLines={8}
+                placeholder="오늘 운동을 계획하고 반성해보세요"
+                onChangeText={description => setdescription(description)}
+                value={description}
+              />
+              <Button
+                onPressButton={HandleSubmit}
+                title="일기전송"
+                color="#2c2c2c"
+              />
+              <Button title="삭제" disabled color="#2c2c2c" />
+            </Form>
+          </View>
+        </Modal>
+      </View>
+      <View>
+        <Text>날짜를 클릭해 일기를 확인해보세요!</Text>
+        <Calendar
+          style={styles.customStyle}
+          // blurRadius={this.modalVisible ? 4: 0}
+          onDayPress={day => {
+            setDate(day.dateString);
+            setModalVisible(true);
+          }}
+        />
+      </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  modal:{
-    alignItems:'center',
 
+  modal:{
+    marginTop:20,
+    alignItems:'center',
+    justifyContent:"center",
   },
   box:{
     width:350,
-    height:350,
+    height:330,
     alignItems:'center',
     backgroundColor:'#FFFFE0'
   },
@@ -94,7 +93,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#e9e9e9',
     justifyContent:"center",
     width:300,
-    height:300,
+    height:200,
     margin:10,
     padding:10
   },
